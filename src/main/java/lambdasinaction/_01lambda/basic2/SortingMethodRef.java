357c967caf7d31d1b2e8005f720bbc0841136f01
mysoyul <vega2k_@naver.com>
2023-11-06 15:23:26 +0900

    SortingRef class add

---
+++ src/main/java/lambdasinaction/_01lambda/basic2/SortingMethodRef.java
@@ -0,0 +1,12 @@
+package lambdasinaction._01lambda.basic2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SortingMethodRef {
+    public static void main(String[] args) {
+        List<Integer> integerList = Arrays.asList(10, 3, 5, 15, 1, 20, 2);
+
+
+    }
+}
