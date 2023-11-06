72977fe418f27ec2833830e7589d3ce08c7e30ff
mysoyul <vega2k_@naver.com>
2023-11-06 15:31:39 +0900

    SortingRef class update

--- src/main/java/lambdasinaction/_01lambda/basic2/SortingMethodRef.java
+++ src/main/java/lambdasinaction/_01lambda/basic2/SortingMethodRef.java
@@ -1,12 +1,28 @@
 package lambdasinaction._01lambda.basic2;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class SortingMethodRef {
     public static void main(String[] args) {
         List<Integer> integerList = Arrays.asList(10, 3, 5, 15, 1, 20, 2);
 
+        //1. Anonymous Inner Class
+        integerList.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        });
+
+        //2. Lambda Expression
+        integerList.sort((n1, n2) -> n1.compareTo(n2));
+
+        //3. Method Reference
+        integerList.sort(Integer::compareTo);
+
+        integerList.forEach(System.out::println);
 
     }
 }
