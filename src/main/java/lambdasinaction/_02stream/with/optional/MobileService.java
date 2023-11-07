0d543f9212f3512fc0d57aa5f3c446345451f9d0
mysoyul <vega2k_@naver.com>
2023-11-07 11:34:10 +0900

    with optional class update

--- src/main/java/lambdasinaction/_02stream/with/optional/MobileService.java
+++ src/main/java/lambdasinaction/_02stream/with/optional/MobileService.java
@@ -5,7 +5,8 @@ import java.util.Optional;
 public class MobileService {
 
 	public Integer getMobileScreenWidth(Optional<Mobile> mobile){
-		return mobile.flatMap(Mobile::getDisplayFeatures)
+		return mobile//.flatMap(mobile1 -> mobile1.getDisplayFeatures())
+					 .flatMap(Mobile::getDisplayFeatures)
 					 .flatMap(DisplayFeatures::getResolution)
 					 .map(ScreenResolution::getWidth)
 					 .orElse(0);
--- src/main/java/lambdasinaction/_02stream/with/optional/MobileTesterWithOptional.java
+++ src/main/java/lambdasinaction/_02stream/with/optional/MobileTesterWithOptional.java
@@ -17,7 +17,18 @@ public class MobileTesterWithOptional {
 		Mobile mobile2 = new Mobile(2015001, "Apple", "iPhone 6s", Optional.empty());		
 		int width2 = mService.getMobileScreenWidth(Optional.of(mobile2));
 		System.out.println("Apple iPhone 16s Screen Width = " + width2);
-		
+
+		//Optional의 orElseGet() 사용
+		Optional<DisplayFeatures> optionalDisplayFeatures = mobile2.getDisplayFeatures();
+		//orElseGet(Supplier) Supplier의 추상메서드 () -> T
+		DisplayFeatures displayFeatures = optionalDisplayFeatures
+				.orElseGet(() -> new DisplayFeatures("0.0", Optional.empty()));
+		System.out.println("displayFeatures size = " + displayFeatures.getSize());
+
+		//OrElseThrow
+		displayFeatures = optionalDisplayFeatures
+				.orElseThrow(() -> new RuntimeException("DisplayFeatures Object is null"));
+
 	}
 
 }
